3a93d3e2e69bf0d3673274c478fab31cfd59c8e7
Martin Arreguin <arrec7281@gmail.com>
2025-06-07 20:14:57 -0600

    update SCRIPTS

--- SCRIPTS/BaseDeDatos.sql
+++ SCRIPTS/BaseDeDatos.sql
@@ -1,6 +1,13 @@
+CREATE SCHEMA IF NOT EXISTS pruebacastores DEFAULT CHARACTER SET utf8mb4;
+USE pruebacastores;
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
+
+--
+-- Estructura de tabla para la tabla `roles`
+--
 
 CREATE TABLE `roles` (
   `id_rol` int(2) NOT NULL,
@@ -8,10 +15,19 @@ CREATE TABLE `roles` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 
+--
+-- Indices de la tabla `roles`
+--
 ALTER TABLE `roles`
   ADD PRIMARY KEY (`id_rol`);
 
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
 
+--
+-- AUTO_INCREMENT de la tabla `roles`
+--
 ALTER TABLE `roles`
   MODIFY `id_rol` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
